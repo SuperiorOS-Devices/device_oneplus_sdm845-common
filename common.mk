a05333c52c301143ac2a243e95eb65023b20123d
optimus9811 <vikasyadav5073@gmail.com>
2023-03-03 13:38:12 +0100

    Init Superior-OS Bring-up
    Misc overlay adaptions/additions/deletions/ los health / live display
    Author: optimus9811 <Optimus9811@github.com>

--- common.mk
+++ common.mk
@@ -30,7 +30,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-superior
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
@@ -270,14 +270,6 @@ PRODUCT_COPY_FILES += \
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
-
-# Lineage Health
-PRODUCT_PACKAGES += \
-    vendor.lineage.health-service.default
-
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.1-service.oneplus_sdm845
 
 # Media
 PRODUCT_PACKAGES += \
